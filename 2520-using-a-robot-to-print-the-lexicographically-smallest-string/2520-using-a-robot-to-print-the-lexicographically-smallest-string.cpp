5a3d96100c66cb1b2b9219633d01178d06b1d09b
Aadarsh Shaheb Singh <114811139+aadarshahebsingh@users.noreply.github.com>
2025-06-06 22:18:42 +0530

    Time: 52 ms (61.88%), Space: 32.6 MB (74.26%) - LeetHub

---
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    char l(vector<int>& freq){
+            for(int i=0;i<26;i++){
+                if(freq[i])return 'a'+i;
+            }
+            return 'a';
+        }
+    string robotWithString(string s) {
+        stack<char> st;
+        string t = "";
+        vector<int> freq(26);
+        for(char ch:s){
+            freq[ch-'a']++;
+        }
+        for(char ch:s){
+            st.push(ch);
+            freq[ch-'a']--;
+
+            while(!st.empty() && st.top()<=l(freq)){
+                t+=st.top();
+                st.pop();
+            }
+        }
+        while(!st.empty()){
+            t+=st.top();
+            st.pop();
+        }
+        return t;
+
+    }
+};
