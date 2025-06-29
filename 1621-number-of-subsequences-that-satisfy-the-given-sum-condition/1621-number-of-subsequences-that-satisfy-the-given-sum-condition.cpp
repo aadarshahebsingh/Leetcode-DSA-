569e0c33ed48055dc45a1168dd79c8e9f512e282
Aadarsh Shaheb Singh <114811139+aadarshahebsingh@users.noreply.github.com>
2025-06-30 02:04:21 +0530

    Time: 28 ms (76.82%), Space: 54.4 MB (14.85%) - LeetHub

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,43 +1,27 @@
-// 3,5,6,7  k=9
-// 3
-// 3,5
-// 3,6
-// 3,5,6
+// 2,3,3,4,6,7   target=12
 
-// 2^3=8-2^2=4
-
-// 2,3,3,4,6,7
-
-// 2^5=32
-// 2^4=16
-// 2^3=8
-// 2^2=4
-// 2^0=1
-
+// sum=9 noOfSub=2^5=32
+// sum=10 noOfSub=2^4=16
+// sum=10 noOfSub=2^3=8
+// sum=11 noOfSub=2^2=4
+// sum=12 noOfSub=2^0=1
 // 61
-
-
-
-
-
 
 
 class Solution {
 public:
-    int numSubseq(vector<int>& arr, int target) {
-        int n=arr.size();
-        int MOD=1e9+7;
+    int numSubseq(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size(),ans=0,MOD=1e9+7;
+        int left=0,right=n-1;
         vector<int> powerOfTwo(n+1,1);
-
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<n+1;i++){
             powerOfTwo[i]=(powerOfTwo[i-1]*2)%MOD;
         }
-        
-        sort(arr.begin(),arr.end());
-        int left=0,right=n-1,ans=0;
-        
+
         while(left<=right){
-            if(arr[left]+arr[right]<=target){
+            
+            if(nums[left]+nums[right]<=target){
                 ans=(ans+powerOfTwo[right-left])%MOD;
                 left++;
             }
@@ -46,27 +30,3 @@ public:
         return ans;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
