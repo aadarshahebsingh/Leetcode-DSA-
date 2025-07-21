ae79f4c4c0669438320148bea91448e752841a64
Aadarsh Shaheb Singh <114811139+aadarshahebsingh@users.noreply.github.com>
2025-07-21 22:18:23 +0530

    Time: 26 ms (64.27%), Space: 44.4 MB (85.03%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     string makeFancyString(string s) {
         if(s.length()<2)return s;
-        int i=0,n=s.length();
+        int i=0;
         string ans="";
-        for( i=0;i<n-2;i++){
+        for(i=0;i<s.length()-2;i++){
             if(s[i]==s[i+1] && s[i]==s[i+2]){
                 continue;
             }
-            else{
-                ans+=s[i];
-            }
+            else ans+=s[i];
         }
-        ans+=s[i];i++;ans+=s[i];
+        ans+=s[i];
+        i++;
+        ans+=s[i];
         return ans;
-
     }
 };
